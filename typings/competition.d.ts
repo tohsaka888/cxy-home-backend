7b28839a13a4dd99c06748435209f1b8d34aa65e
tohsaka888 <156132264@qq.com>
2022-09-06 16:00:54 +0800

    🦄 refactor(type): 比赛type

---
+++ typings/competition.d.ts
@@ -0,0 +1,64 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-05 17:07:08
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-05 17:07:09
+ * @Description: 请填写简介
+ */
+
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-05 13:39:24
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-05 13:39:24
+ * @Description: 请填写简介
+ */
+
+declare namespace Competition {
+
+  type Participant = {
+    username: string;
+    email: string;
+  }
+
+  type CompetitionImage = {
+    intro: string;
+    url: string;
+  }
+
+  type Award = {
+    award: string;
+  }
+
+  type AwardSetting = {
+    award: string;
+    limit: number;
+  }
+
+  type Winner = Participant & Award;
+
+  type Creator = Participant;
+
+  type Info = {
+    place: string;
+    way: string;
+    limit: number;
+    signUpStart: string;
+    signUpEnd: string;
+    time: string;
+    duration: string;
+  }
+
+  type Competition = {
+    name: string;
+    createdTime: string;
+    updatedTime: string;
+    participants: Participant[];
+    winners: Winner[];
+    intro: string;
+    info: Info;
+    banners: CompetitionImage[];
+    creator: Creator;
+    awardSetting: AwardSetting[];
+  }
+}
