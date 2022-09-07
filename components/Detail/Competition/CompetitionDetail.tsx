697564668bb37275a133cf265f71ef7ab642edd7
tohsaka888 <156132264@qq.com>
2022-09-07 16:24:59 +0800

    ✨ feat: 新增/修改/查看详情页

---
+++ components/Detail/Competition/CompetitionDetail.tsx
@@ -0,0 +1,327 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-07 11:35:44
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-07 16:13:43
+ * @Description: 请填写简介
+ */
+import { Col, DatePicker, Form, Input, Layout, message, Row, Typography } from 'antd'
+import Header from 'components/Header'
+import useCompetitionDetail from 'hooks/useCompetitionDetail'
+import moment from 'moment'
+import { useRouter } from 'next/router'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import styles from './index.module.css'
+
+const initCompetition = {
+  name: '',
+  createdTime: '',
+  creator: {
+    username: '',
+    email: '',
+  },
+  updatedTime: '',
+  participants: [],
+  awardSetting: [{ award: '一等奖', limit: 0 }, { award: '二等奖', limit: 0 }, { award: '三等奖', limit: 0 }, { award: '优秀奖', limit: 0 }],
+  winners: [],
+  intro: '',
+  info: {
+    place: '',
+    way: '',
+    signUpStart: '',
+    signUpEnd: '',
+    limit: 0,
+    time: '',
+    duration: '0小时',
+  },
+  banners: [],
+}
+
+function CompetitionDetail() {
+  const router = useRouter()
+  const { id, token, way } = router.query
+  const [competition, setCompetition] = useState<(Competition.Competition & { _id?: string }) | null>(null)
+  const onSuccess = (data: any) => {
+    console.log(data)
+    setCompetition(data.success ? data.competition : initCompetition)
+  }
+
+  const formattedCompetition = useMemo(() => {
+    return competition ? {
+      ...competition,
+      info: {
+        ...competition.info,
+        signUpStart: competition.info.signUpStart ? moment(competition.info.signUpStart) : undefined,
+        signUpEnd: competition.info.signUpEnd ? moment(competition.info.signUpEnd) : undefined,
+        time: competition.info.time ? moment(competition.info.time) : undefined,
+        duration: +competition.info.duration.split('小时')[0]
+      }
+    } : undefined
+  }, [competition])
+
+  useEffect(() => {
+    if (way === 'add') {
+      setCompetition(initCompetition)
+    }
+  }, [way])
+
+  const onError = useCallback((error: any) => {
+    message.error(error)
+  }, [])
+  useCompetitionDetail({ way: (way as string), id: (id as string) || '', onSuccess, onError })
+
+  return (
+    <>
+      <Header />
+      <Layout>
+        <Layout.Content>
+          <div className={styles['container']}>
+            {competition && competition.name && <div className={styles['part-container']}>
+              <div className={styles['title']}>{competition.name}</div>
+            </div>}
+
+            <div className={styles['part-container']}>
+              <div className={styles['part-title']}>基本信息</div>
+              {competition &&
+                <Form labelCol={{ span: 9 }} wrapperCol={{ span: 15 }} initialValues={formattedCompetition}>
+                  <Row style={{ marginTop: '16px' }}>
+                    <Col span={8}>
+                      <Form.Item label={"比赛名称"} name={'name'} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入比赛名称' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.name
+                          : <Input placeholder='请输入比赛名称' value={competition.name} />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"创建时间"} name={'createdTime'} style={{ marginBottom: '16px' }}>
+                        {way === 'view'
+                          ? competition?.createdTime
+                          : <Input placeholder='请输入创建时间' value={competition?.createdTime} disabled />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"更新时间"} name={'updatedTime'} style={{ marginBottom: '16px' }}>
+                        {way === 'view'
+                          ? competition?.updatedTime
+                          : <Input placeholder='请输入更新时间' value={competition?.updatedTime} disabled />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"举办地点"} name={['info', 'place']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入举办地点' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.place
+                          : <Input
+                            placeholder='请输入举办地点'
+                            value={competition?.info.place}
+                            onChange={(e) => setCompetition({ ...competition, info: { ...competition.info, place: e.target.value } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"比赛方式"} name={['info', 'way']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入比赛方式' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.way
+                          : <Input
+                            placeholder='请输入比赛方式'
+                            value={competition?.info.way}
+                            onChange={(e) => setCompetition({ ...competition, info: { ...competition.info, way: e.target.value } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"限报人数"} name={['info', 'limit']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入比赛方式' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.limit
+                          : <Input
+                            placeholder='请输入比赛方式'
+                            value={competition?.info.limit}
+                            type={'number'}
+                            onChange={(e) => setCompetition({ ...competition, info: { ...competition.info, limit: +e.target.value } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"开始报名时间"} name={['info', 'signUpStart']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入开始报名时间' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.signUpStart
+                          : <DatePicker
+                            placeholder='请选择开始报名时间'
+                            format={'YYYY-MM-DD hh:mm:ss'}
+                            style={{ width: '100%' }}
+                            onChange={(date) => setCompetition({ ...competition, info: { ...competition.info, signUpStart: date?.format('YYYY-MM-DD hh:mm:ss') || '' } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"结束报名时间"} name={['info', 'signUpEnd']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入结束报名时间' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.signUpEnd
+                          : <DatePicker
+                            placeholder='请选择结束报名时间'
+                            style={{ width: '100%' }}
+                            format={'YYYY-MM-DD hh:mm:ss'}
+                            onChange={(date) => setCompetition({ ...competition, info: { ...competition.info, signUpEnd: date?.format('YYYY-MM-DD hh:mm:ss') || '' } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"比赛时间"} name={['info', 'time']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入比赛时间' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.time
+                          : <DatePicker
+                            placeholder='请选择比赛时间'
+                            style={{ width: '100%' }}
+                            format={'YYYY-MM-DD hh:mm:ss'}
+                            showTime={{ defaultValue: moment('00:00:00', 'HH:mm:ss') }}
+                            onChange={(date) => setCompetition({ ...competition, info: { ...competition.info, time: date?.format('YYYY-MM-DD hh:mm:ss') || '' } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"比赛时长"} name={['info', 'duration']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入比赛方式' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.info.duration
+                          : <Input
+                            placeholder='请输入时长'
+                            value={competition?.info.duration}
+                            type={'number'}
+                            suffix={'小时'}
+                            onChange={(e) => setCompetition({ ...competition, info: { ...competition.info, duration: e.target.value + '小时' } })}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={24}>
+                      <Form.Item label={'介绍'} name={['intro']} labelCol={{ span: 3 }} wrapperCol={{ span: 21 }} style={{ marginBottom: '8px' }}
+                        rules={[{ required: true, message: '请输入比赛方式' }]}
+                      >
+                        {way === 'view'
+                          ? (
+                            <Typography style={{ marginTop: '4px' }}>
+                              <Typography.Paragraph ellipsis={{ rows: 5, expandable: true }}>
+                                {competition.intro}
+                              </Typography.Paragraph>
+                            </Typography>
+                          )
+                          : <Input.TextArea rows={5} />
+                        }
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form>
+              }
+            </div>
+            <div className={styles['part-container']}>
+              <div className={styles['part-title']}>奖项设置</div>
+              {competition && <>
+                <Form labelCol={{ span: 9 }} wrapperCol={{ span: 15 }} initialValues={formattedCompetition}>
+                  <Row style={{ marginTop: '16px' }}>
+                    <Col span={8}>
+                      <Form.Item label={"一等奖"} name={['awardSetting', '0', 'limit']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入人数' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.awardSetting[0].limit
+                          : <Input
+                            placeholder='请输入人数'
+                            value={competition?.awardSetting[0].limit}
+                            type={'number'}
+                            onChange={(e) => {
+                              competition.awardSetting[0].limit = +e.target.value
+                              setCompetition({ ...competition })
+                            }}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"二等奖"} name={['awardSetting', '1', 'limit']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入人数' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.awardSetting[1].limit
+                          : <Input
+                            placeholder='请输入人数'
+                            value={competition?.awardSetting[1].limit}
+                            type={'number'}
+                            onChange={(e) => {
+                              competition.awardSetting[1].limit = +e.target.value
+                              setCompetition({ ...competition })
+                            }}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"三等奖"} name={['awardSetting', '2', 'limit']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入人数' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.awardSetting[2].limit
+                          : <Input
+                            placeholder='请输入人数'
+                            value={competition?.awardSetting[2].limit}
+                            type={'number'}
+                            onChange={(e) => {
+                              competition.awardSetting[2].limit = +e.target.value
+                              setCompetition({ ...competition })
+                            }}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item label={"优秀奖"} name={['awardSetting', '3', 'limit']} style={{ marginBottom: '16px' }}
+                        rules={[{ required: true, message: '请输入人数' }]}
+                      >
+                        {way === 'view'
+                          ? competition?.awardSetting[3].limit
+                          : <Input
+                            placeholder='请输入人数'
+                            value={competition?.awardSetting[3].limit}
+                            type={'number'}
+                            onChange={(e) => {
+                              competition.awardSetting[3].limit = +e.target.value
+                              setCompetition({ ...competition })
+                            }}
+                          />
+                        }
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </Form>
+              </>}
+            </div>
+          </div>
+        </Layout.Content>
+      </Layout>
+    </>
+  )
+}
+
+export default CompetitionDetail
