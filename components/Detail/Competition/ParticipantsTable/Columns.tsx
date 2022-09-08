e83134aeb10acd0a9896b53ac0c0d872b1232e94
tohsaka888 <156132264@qq.com>
2022-09-08 17:09:29 +0800

    ✨ feat: 联动删除获奖名单

--- components/Detail/Competition/ParticipantsTable/Columns.tsx
+++ components/Detail/Competition/ParticipantsTable/Columns.tsx
@@ -2,9 +2,30 @@
  * @Author: tohsaka888
  * @Date: 2022-09-08 10:21:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-08 10:25:40
+ * @LastEditTime: 2022-09-08 17:06:59
  * @Description: 请填写简介
  */
+
+import { Popconfirm } from "antd"
+import { CompetitionContext } from "context/context"
+import { useContext, useCallback } from "react"
+
+export const Operation = ({ record }: { record: any }) => {
+  const { competition, setCompetition } = useContext(CompetitionContext)!
+
+  const deleteItem = useCallback(() => {
+    if (competition) {
+      const newParticipants = competition?.participants.filter(participant => participant.username !== record.username)
+      const newWinners = competition?.winners.filter(winner => winner.username !== record.username)
+      setCompetition({ ...competition, participants: newParticipants, winners: newWinners })
+    }
+  }, [competition, record.username, setCompetition])
+  return (
+    <Popconfirm okText={"确认"} cancelText={"取消"} onConfirm={deleteItem} title={'确认删除,删除后无法恢复'}>
+      <a>删除</a>
+    </Popconfirm>
+  )
+}
 
 export const columns = [
   {
@@ -28,12 +49,8 @@ export const columns = [
   {
     title: '操作',
     key: 'operation',
-    render() {
-      return (
-        <>
-          <a>删除</a>
-        </>
-      )
+    render(text: any, record: any) {
+      return <Operation record={record} />
     }
   }
 ]
