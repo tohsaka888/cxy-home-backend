816714c1866c81059ec1d5af7d3dffd9eaa79332
tohsaka888 <156132264@qq.com>
2022-09-08 11:54:23 +0800

    ✨ feat: 提取公用fetcher

--- hooks/useCompetitionDetail.tsx
+++ hooks/useCompetitionDetail.tsx
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-09-07 11:18:07
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-07 16:13:21
+ * @LastEditTime: 2022-09-08 11:37:37
  * @Description: 请填写简介
  */
 
@@ -17,12 +17,14 @@ type Props = {
   onError?: (err: any, key: string) => void
 }
 
+export const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
 function useCompetitionDetail({ way, id, onSuccess, onError }: Props) {
-  const { data, error } = useSWR(way !== 'add' ? `${competitionUrl}/api/competition/${id}` : null, async (url: string) => {
-    const res = await fetch(url)
-    const data = await res.json()
-    return data
-  }, {
+  const { data, error } = useSWR(way !== 'add' ? `${competitionUrl}/api/competition/${id}` : null, fetcher, {
     onSuccess: onSuccess,
     onError: onError || (() => { })
   })
