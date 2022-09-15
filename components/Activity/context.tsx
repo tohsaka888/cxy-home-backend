8ed9b306774fa65e6d7c510f8d543ccad28a7917
tohsaka888 <156132264@qq.com>
2022-09-15 11:55:24 +0800

    ✨ feat: context

---
+++ components/Activity/context.tsx
@@ -0,0 +1,23 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-15 09:51:06
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-15 09:51:07
+ * @Description: 请填写简介
+ */
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-15 08:45:51
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-15 08:46:53
+ * @Description: 请填写简介
+ */
+
+import { createContext } from "react";
+
+type LoadingContextProps = {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const LoadingContext = createContext<LoadingContextProps | null>(null)
