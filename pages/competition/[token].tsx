df8d85a50de666a9ef0a4f3b5b33dab1c99e89ea
tohsaka888 <156132264@qq.com>
2022-10-06 09:09:08 +0800

    ✨ feat: 增加权限判断

--- pages/competition/[token].tsx
+++ pages/competition/[token].tsx
@@ -6,28 +6,63 @@
  * @Description: 请填写简介
  */
 
-import { Layout } from 'antd'
-import React from 'react'
-import CompetitionList from '../../components/Competition'
-import Header from '../../components/Header'
+import { Button, Layout, Result } from "antd";
+import useLoginStatus from "hooks/useLoginStatus";
+import { useRouter } from "next/router";
+import React, { useMemo, useState } from "react";
+import CompetitionList from "../../components/Competition";
+import Header from "../../components/Header";
 
 function Competition() {
+  const { data } = useLoginStatus();
+  const { push } = useRouter();
+
+  const hasPermission = useMemo(() => {
+    if (
+      data.result.permissions.includes("管理员") ||
+      data.result.permissions.includes("比赛")
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }, [data.result.permissions]);
+
   return (
     <Layout>
       <Header />
       <Layout>
         <Layout.Content
           style={{
-            minHeight: 'calc(100vh - 65px)',
-            width: '70vw',
-            marginLeft: '15vw'
+            minHeight: "calc(100vh - 65px)",
+            width: "70vw",
+            marginLeft: "15vw",
           }}
         >
-          <CompetitionList />
+          {hasPermission ? (
+            <CompetitionList />
+          ) : (
+            <Result
+              status="403"
+              title="403"
+              subTitle="对不起,您无权操作!"
+              extra={
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    localStorage.clear();
+                    push("/");
+                  }}
+                >
+                  Back Home
+                </Button>
+              }
+            />
+          )}
         </Layout.Content>
       </Layout>
     </Layout>
-  )
+  );
 }
 
-export default Competition
+export default Competition;
