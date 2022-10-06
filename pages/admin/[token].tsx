695dde19cd6ca548759d03a138f6f18f11eb9833
tohsaka888 <156132264@qq.com>
2022-10-06 09:08:45 +0800

    ✨ feat: 增加权限判断

--- pages/admin/[token].tsx
+++ pages/admin/[token].tsx
@@ -6,16 +6,50 @@
  * @Description: 请填写简介
  */
 
-import { Layout } from 'antd'
-import React from 'react'
-import Header from '../../components/Header'
+import { Button, Layout, Result } from "antd";
+import useLoginStatus from "hooks/useLoginStatus";
+import { useRouter } from "next/router";
+import React, { useMemo } from "react";
+import Header from "../../components/Header";
 
 function Admin() {
+  const { data } = useLoginStatus();
+  const { push } = useRouter();
+
+  const hasPermission = useMemo(() => {
+    if (data.result.permissions.includes("管理员")) {
+      return true;
+    } else {
+      return false;
+    }
+  }, [data.result.permissions]);
   return (
     <Layout>
       <Header />
+      {hasPermission ? (
+        <>
+        
+        </>
+      ) : (
+        <Result
+          status="403"
+          title="403"
+          subTitle="对不起,您无权操作!"
+          extra={
+            <Button
+              type="primary"
+              onClick={() => {
+                localStorage.clear();
+                push("/");
+              }}
+            >
+              Back Home
+            </Button>
+          }
+        />
+      )}
     </Layout>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
