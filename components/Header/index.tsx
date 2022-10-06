6f4891b85bcbdeff3e2819ce9423dbaeb0e1043f
tohsaka888 <156132264@qq.com>
2022-10-06 10:32:06 +0800

    ✨ feat: 增加退出登录

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,33 +6,56 @@
  * @Description: 请填写简介
  */
 
-import { Layout, Menu } from 'antd'
-import { useRouter } from 'next/router'
-import React, { useEffect, useRef } from 'react'
+import { Button, Layout, Menu } from "antd";
+import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
 
 export const menuItems = [
-  { label: '比赛模块', key: 'competition' },
-  { label: '管理员模块', key: 'admin' },
-  { label: '活动模块', key: 'activity' }
-]
+  { label: "比赛模块", key: "competition" },
+  { label: "管理员模块", key: "admin" },
+  { label: "活动模块", key: "activity" },
+];
 
 function Header() {
-  const router = useRouter()
-  const path = router.pathname.split('/')[1]
-  const tokenRef = useRef<string>('')
+  const router = useRouter();
+  const path = router.pathname.split("/")[1];
+  const tokenRef = useRef<string>("");
 
   useEffect(() => {
-    tokenRef.current = localStorage.getItem('token') || ''
-  }, [])
+    tokenRef.current = localStorage.getItem("token") || "";
+  }, []);
   return (
-    <Layout.Header style={{ position: 'sticky', top: '0px', zIndex: 999 }}>
-      <Menu items={menuItems} mode={"horizontal"} theme={"dark"} defaultSelectedKeys={[path]}
+    <Layout.Header
+      style={{
+        position: "sticky",
+        top: "0px",
+        zIndex: 999,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Menu
+        items={menuItems}
+        mode={"horizontal"}
+        theme={"dark"}
+        defaultSelectedKeys={[path]}
         onClick={(info) => {
-          router.push(`/${info.key}/${tokenRef.current}`)
+          router.push(`/${info.key}/${tokenRef.current}`);
         }}
       />
+      <Button
+        type="primary"
+        danger
+        onClick={() => {
+          localStorage.clear();
+          router.push("/");
+        }}
+      >
+        退出登录
+      </Button>
     </Layout.Header>
-  )
+  );
 }
 
-export default Header
+export default Header;
