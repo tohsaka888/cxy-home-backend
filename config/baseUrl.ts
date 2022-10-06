bde0d91be3b230e0f27c6f0bc12750da7a077359
tohsaka888 <156132264@qq.com>
2022-10-06 09:08:15 +0800

    ✨ feat: 修改主路由

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -6,7 +6,8 @@
  * @Description: 请填写简介
  */
 
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home-backend.netlify.app'
-export const activityUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cxy-home-backend-activity.netlify.app'
-export const competitionUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : 'https://cxy-home-backend-competition.netlify.app'
-export const loginUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3003' : 'https://cxy-home-backend-login.netlify.app'
+export const baseUrl = "https://cxy-home-backend.netlify.app";
+export const activityUrl = "https://cxy-home-backend-activity.netlify.app";
+export const competitionUrl =
+  "https://cxy-home-backend-competition.netlify.app";
+export const loginUrl = "https://cxy-home-backend-login.netlify.app";
